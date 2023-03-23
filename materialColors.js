f49323bb55f48913d13abbd7c4a6965ed20bf0b0
Sketch <54947833+lukeplays33@users.noreply.github.com>
2023-03-23 19:48:13 +0100

    Update materialColors.js

--- materialColors.js
+++ materialColors.js
@@ -1,13 +1,6 @@
   import { Hct, argbFromHex, themeFromSourceColor, applyTheme  } from "https://cdn.skypack.dev/@material/material-color-utilities@0.2.4";
 
 window.setInterval(function () {
-
-// Simple demonstration of HCT.
-const color = Hct.fromInt(0xff4285f4);
-console.log(`Hue: ${color.hue}`);
-console.log(`Chrome: ${color.chroma}`);
-console.log(`Tone: ${color.tone}`);
-
     var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
 // Get the theme from a hex color
@@ -18,9 +11,6 @@ const theme = themeFromSourceColor(argbFromHex(metaThemeColor.getAttribute("cont
     blend: true,
   },
 ]);
-
-// Print out the theme as JSON
-console.log(JSON.stringify(theme, null, 2));
 
 // Check if the user has dark mode turned on
 const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
